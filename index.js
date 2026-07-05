3492602da5d9a8b5895daf10823147656facc127
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-07-05 11:35:11 +0530

    fix: update response messages for HTTP methods in index.js

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello, sdfdsf!");
+    res.send("Get Request Received!");
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send("Post Request Received!");
+});
+
+app.put("/", (req, res) => {
+    res.send("Put Request Received!");
+});
+
+app.delete("/", (req, res) => {
+    res.send("Delete Request Received!");
 });
 
 app.listen(3000, () => {
