bef176e9c12765aa01293a460c8029aa5c213f3c
Nadeeja Amaraweera <nadeejaamaraweera@gmail.com>
2026-07-11 16:34:53 +0530

    feat: implement Student model and update POST route to save student data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
+import Student from "./models/Student.js";
 
 const app = express();
 
-const mongo_URI = "mongodb+srv://admin:1234@cluster0.9kpoisv.mongodb.net/?appName=Cluster0";
+const mongo_URI = "mongodb+srv://admin:1234@cluster0.9kpoisv.mongodb.net/icomputers?appName=Cluster0";
 
 mongoose.connect(mongo_URI).then(
     () => {
@@ -16,33 +17,23 @@ mongoose.connect(mongo_URI).then(
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Get Request Received!"
-    });
-});
-
 app.post("/", (req, res) => {
     console.log(req.body);
-    res.json({
-        message: "Post Request Received!",
-        data: req.body
+
+    const newStudent = new Student({
+        name: req.body.name,
+        address: req.body.address
     });
+
+    newStudent.save().then(
+        ()=>{
+            res.json({
+                message: "Student Saved Successfully!"
+            });
+        }
+    );
+
 }); 
-
-app.put("/", (req, res) => {
-    res.json({
-        message: "Put Request Received!",
-        data: req.body
-    });
-});
-
-app.delete("/", (req, res) => {
-    res.json({
-        message: "Delete Request Received!",
-        data: req.body
-    });
-});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
